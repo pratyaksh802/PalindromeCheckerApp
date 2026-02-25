bd777501d26d26889e0b2d35cc51d603b0cf2cd9
Pratyaksh Singh <ps2611@srmist.edu.in>
2026-02-25 11:21:14 +0530

    UC1: Final updates

---
+++ srcmain.java
@@ -0,0 +1,11 @@
+public class PalindromeCheckerApp {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Palindrome Checker App");
+        System.out.println("Version 1.0");
+        System.out.println("Welcome to the application!");
+
+    }
+}
